c1016ff5fedf52d39f7033404c9a50dfd88187c3
Vladislav Kolegov <v.kolegov@outlook.com>
2021-10-30 18:20:32 +0700

    Отслеживание сбережений; .env -> settings.cfg

--- app.go
+++ app.go
@@ -29,7 +29,7 @@ func main() {
 		command string
 	)
 
-	err = godotenv.Load(".env")
+	err = godotenv.Load("settings.cfg")
 
 	if err != nil {
 		panic(err)
@@ -38,13 +38,13 @@ func main() {
 	godName = os.Getenv("GODNAME")
 
 	if godName == "" {
-		panic("Укажите имя бога в переменной окружения или .env файле")
+		panic("Укажите имя бога в переменной окружения или settings.cfg файле")
 	}
 
 	key = os.Getenv("KEY")
 
 	if key == "" {
-		panic("Укажите API токен в переменной окружения или .env файле")
+		panic("Укажите API токен в переменной окружения или settings.cfg файле")
 	}
 
 	fullUrl = apiUrl + godName + "/" + key
@@ -118,22 +118,13 @@ func trackActivity(url string, rate int) {
 			trackWood()
 		}
 
+		if currentData.Savings != "" {
+			trackSavings()
+		}
+
 		time.Sleep(time.Second * time.Duration(rate))
 	}
 }
-
-var (
-	lastDiaryEntry string
-
-	lastHealth uint16 = 0
-	lastPrana  uint8  = 0
-	lastGold   string
-	lastPillar uint16 = 0
-	lastTown   string
-
-	lastBrickCnt int16 = -1
-	lastWoodCnt  int32 = -1
-)
 
 func greetings() {
 
@@ -180,91 +171,5 @@ func greetings() {
 			"Ковчег достроен %s, поздравляем!\n",
 			templeDate.Format(dateFormat),
 		)
-	}
-}
-
-func trackBasicData() {
-
-	var (
-		whereabouts string
-	)
-
-	if lastDiaryEntry != currentData.DiaryLast {
-		lastDiaryEntry = currentData.DiaryLast
-		fmt.Printf("[Дневник] %s\n", lastDiaryEntry)
-	}
-
-	if lastHealth != currentData.Health ||
-		lastPrana != currentData.Godpower ||
-		lastPillar != currentData.Distance ||
-		lastTown != currentData.TownName ||
-		lastGold != currentData.GoldApprox {
-
-		if currentData.TownName == "" {
-			whereabouts = fmt.Sprintf("Столб #%d", currentData.Distance)
-		} else {
-			whereabouts = fmt.Sprintf("%s (ст. %d)", currentData.TownName, currentData.Distance)
-		}
-
-		fmt.Printf(
-			"[%s] Здоровье: %d/%d; Прана: %d%%; Золота: %s",
-			whereabouts,
-			currentData.Health,
-			currentData.MaxHealth,
-			currentData.Godpower,
-			currentData.GoldApprox,
-		)
-
-		if currentData.BricksCnt < 1000 {
-			fmt.Printf("; Золотых кирпичей: %d/1000", currentData.BricksCnt)
-		}
-
-		if currentData.WoodCnt < 1000 {
-			fmt.Printf("; Дерева: %d/1000", currentData.WoodCnt)
-		}
-
-		fmt.Print("\n")
-
-		lastHealth = currentData.Health
-		lastPrana = currentData.Godpower
-		lastTown = currentData.TownName
-		lastPillar = currentData.Distance
-	}
-
-}
-
-func trackBricks() {
-
-	var bCnt, diff int16
-	bCnt = int16(currentData.BricksCnt)
-
-	if lastWoodCnt == -1 {
-		lastBrickCnt = bCnt
-	} else if lastBrickCnt != bCnt {
-		diff = bCnt - lastBrickCnt
-		lastBrickCnt = bCnt
-		fmt.Printf("%s получил %d кирпичей!\n", currentData.Name, diff)
-	}
-
-	if currentData.BricksCnt == 1000 {
-		fmt.Printf("%s собрал все кирпичи для постройки храма!\n", currentData.Name)
-	}
-}
-
-func trackWood() {
-
-	var wCnt, diff int32
-	wCnt = int32(currentData.WoodCnt)
-
-	if lastWoodCnt == -1 {
-		lastWoodCnt = wCnt
-	} else if lastWoodCnt != wCnt {
-		diff = wCnt - lastWoodCnt
-		lastWoodCnt = wCnt
-		fmt.Printf("Герой получил %d досок для ковчега!\n", diff)
-	}
-
-	if currentData.WoodCnt == 1000 {
-		fmt.Printf("%s собрал дерево для постройки ковчега!\n", currentData.Name)
 	}
 }
---
+++ tracking.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	lastDiaryEntry string
+
+	lastHealth uint16 = 0
+	lastPrana  uint8  = 0
+	lastGold   string
+	lastPillar uint16 = 0
+	lastTown   string
+
+	lastBrickCnt int16 = -1
+	lastWoodCnt  int32 = -1
+
+	lastSavings int32 = -1
+)
+
+func trackBasicData() {
+
+	var (
+		whereabouts string
+	)
+
+	if lastDiaryEntry != currentData.DiaryLast {
+		lastDiaryEntry = currentData.DiaryLast
+		fmt.Printf("[Дневник] %s\n", lastDiaryEntry)
+	}
+
+	if lastHealth != currentData.Health ||
+		lastPrana != currentData.Godpower ||
+		lastPillar != currentData.Distance ||
+		lastTown != currentData.TownName ||
+		lastGold != currentData.GoldApprox {
+
+		if currentData.TownName == "" {
+			whereabouts = fmt.Sprintf("Столб #%d", currentData.Distance)
+		} else {
+			whereabouts = fmt.Sprintf("%s (ст. %d)", currentData.TownName, currentData.Distance)
+		}
+
+		fmt.Printf(
+			"[%s] Здоровье: %d/%d; Прана: %d%%; Золота: %s",
+			whereabouts,
+			currentData.Health,
+			currentData.MaxHealth,
+			currentData.Godpower,
+			currentData.GoldApprox,
+		)
+
+		if currentData.Savings != "" {
+			fmt.Printf("; Сбережений: %s", currentData.Savings)
+		}
+
+		if currentData.BricksCnt < 1000 {
+			fmt.Printf("; Золотых кирпичей: %d/1000", currentData.BricksCnt)
+		}
+
+		if currentData.WoodCnt < 1000 {
+			fmt.Printf("; Дерева: %d/1000", currentData.WoodCnt)
+		}
+
+		fmt.Print("\n")
+
+		lastHealth = currentData.Health
+		lastPrana = currentData.Godpower
+		lastTown = currentData.TownName
+		lastPillar = currentData.Distance
+	}
+
+}
+
+func trackBricks() {
+
+	var bCnt, diff int16
+	bCnt = int16(currentData.BricksCnt)
+
+	if lastWoodCnt == -1 {
+		lastBrickCnt = bCnt
+	} else if lastBrickCnt != bCnt {
+		diff = bCnt - lastBrickCnt
+		lastBrickCnt = bCnt
+		fmt.Printf("%s получил %d кирпичей!\n", currentData.Name, diff)
+	}
+
+	if currentData.BricksCnt == 1000 {
+		fmt.Printf("%s собрал все кирпичи для постройки храма!\n", currentData.Name)
+	}
+}
+
+func trackWood() {
+
+	var wCnt, diff int32
+	wCnt = int32(currentData.WoodCnt)
+
+	if lastWoodCnt == -1 {
+		lastWoodCnt = wCnt
+	} else if lastWoodCnt != wCnt {
+		diff = wCnt - lastWoodCnt
+		lastWoodCnt = wCnt
+		fmt.Printf("Герой получил %d досок для ковчега!\n", diff)
+	}
+
+	if currentData.WoodCnt == 1000 {
+		fmt.Printf("%s собрал дерево для постройки ковчега!\n", currentData.Name)
+	}
+}
+
+func trackSavings() {
+
+	savings, err := parseSavings(currentData.Savings)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if lastSavings == -1 {
+		lastSavings = savings
+	} else if lastSavings != savings {
+
+		diff := savings - lastSavings
+
+		fmt.Printf("Герой отложил %d тысяч!", diff)
+	}
+}
+func parseSavings(savingsString string) (int32, error) {
+	var (
+		savings = 0
+		a       string // a = тысяч
+	)
+	_, err := fmt.Sscanf(savingsString, "%d %s", &savings, &a)
+
+	if err != nil {
+
+		return -1, errors.New(
+			fmt.Sprintf(
+				"Ошибка при парсинге сбережений \"%s\": %s\n", savingsString, err.Error(),
+			),
+		)
+	}
+
+	return int32(savings), nil
+}
